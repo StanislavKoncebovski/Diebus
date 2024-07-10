618a7362b7c113ac8a7d748cea1f741d9335cc4e
Stanislav <stanislav@pikkatech.eu>
2024-07-10 23:07:13 +0200

    Code\astro.py by Paul Schlyter with example calculates

--- Code/astro.py
+++ Code/astro.py
@@ -65,7 +65,7 @@ def solar_polar_coordinates(d: float) -> (float, float):
     r = math.sqrt(x**2 + y**2)
     v = math.atan2(y, x)
 
-    return r, v
+    return r, v * 180 / math.pi
 
 def solar_longitude(d: float) -> float:
     w = longitude_of_perihelion(d)
@@ -76,8 +76,10 @@ def solar_longitude(d: float) -> float:
 def solar_ecliptic_coordinates(d: float) -> (float, float, float):
     r, v = solar_polar_coordinates(d)
     lon = solar_longitude(d)
-    x = r * math.cos(lon)
-    y = r * math.sin(lon)
+    lon = tools.fmod(lon, 360)
+
+    x = r * math.cos(lon * math.pi / 180)
+    y = r * math.sin(lon * math.pi / 180)
     z = 0
 
     return x, y, z
@@ -109,8 +111,8 @@ def solar_polar_equatorial_coordinates(d: float) -> (float, float):
     """
     x_equat, y_equat, z_equat = solar_cartesian_equatorial_coordinates(d)
     r = math.sqrt(x_equat**2 + y_equat**2 + z_equat**2)
-    RA = math.atan2(y_equat, x_equat)
-    decl = math.asin(z_equat / r)
+    RA = math.atan2(y_equat, x_equat) * 180 / math.pi
+    decl = math.asin(z_equat / r) * 180 / math.pi
 
     return RA, decl
 
@@ -119,7 +121,9 @@ def greenwich_sidereal_time(d: float) -> float:
     :param d:
     :return: Sidereal time at Greenwich, hours
     """
-    return mean_solar_longitude(d) / 15 + 12
+    L = mean_solar_longitude(d)
+    L = tools.fmod(L, 360)
+    return L / 15 + 12
 
 def local_sidereal_time(d: float, longitude: float):
     return greenwich_sidereal_time(d) + longitude / 15
@@ -129,16 +133,18 @@ def alternative_polar_equatorial_coordinates(d: float, longitude: float) -> (flo
     Equatorial coordinates where RA is replaced with the hour angle
     :param d:
     :param longitude:
-    :return: HA, declination
+    :return: HA (hours), declination
     """
     RA, decl = solar_polar_equatorial_coordinates(d)
     sid = greenwich_sidereal_time(d) + longitude / 15
-    RA, decl = solar_polar_equatorial_coordinates(d)
 
-    return sid - RA, decl
+    return sid - RA / 15, decl
 
 def solar_horizontal_coordinates(d: float, latitude: float, longitude: float) -> (float, float):
     HA, decl = alternative_polar_equatorial_coordinates(d, longitude)
+
+    HA *= 15 # convert from hours to degrees
+
     x = math.cos(HA * math.pi / 180) * math.cos(decl * math.pi / 180)
     y = math.sin(HA * math.pi / 180) * math.cos(decl * math.pi / 180)
     z = math.sin(decl * math.pi / 180)
@@ -147,7 +153,67 @@ def solar_horizontal_coordinates(d: float, latitude: float, longitude: float) ->
     y_hor = y
     z_hor = x * math.cos(latitude * math.pi / 180) + z * math.sin(latitude * math.pi / 180)
 
-    azimuth = math.atan2(y_hor, x_hor) + 180
-    altitude = math.asin(z_hor)
+    azimuth = math.atan2(y_hor, x_hor) * 180 / math.pi + 180
+    altitude = math.asin(z_hor) * 180 / math.pi
 
     return azimuth, altitude
+
+if __name__ == '__main__':
+    t = 2448000.5  # 1990-04-14 00:00 UT
+    d = day_number(t)
+
+    print(f"t = {t}, d = {d}")
+
+    w = longitude_of_perihelion(d)
+    print(f"longitude_of_perihelion = {w}")
+    e = eccentricity(d)
+    M = mean_solar_anomaly(d)
+    M = tools.fmod(M, 360)
+
+    print(f"eccentricity = {e}")
+    print(f"mean_solar_anomaly = {M}")
+
+    epsilon = obliquity(d)
+    L = mean_solar_longitude(d)
+    L = tools.fmod(L, 360)
+
+    print(f"obliquity = {epsilon}")
+    print(f"mean_solar_longitude = {L}")
+
+    E = eccentric_solar_anomaly(d)
+    E = tools.fmod(E, 360)
+    print(f"eccentric_solar_anomaly = {E}")
+
+    x, y = solar_cartesian_coordinates(d)
+
+    print(f"Solar Cartesian coordinates: x = {x}, y = {y}")
+
+    r, v = solar_polar_coordinates(d)
+    print(f"Solar polar coordinates: r = {r}, v = {v}")
+
+    lon = solar_longitude(d)
+    lon = tools.fmod(lon, 360)
+    print(f"solar_longitude = {lon}")
+
+    x, y, z = solar_ecliptic_coordinates(d)
+    print(f"solar_ecliptic_coordinates: x = {x}, y = {y}, z = {z}")
+
+    x_equat, y_equat, z_equat = solar_cartesian_equatorial_coordinates(d)
+    print(f"solar_cartesian_equatorial_coordinates: x = {x_equat}, y = {y_equat}, z = {z_equat}")
+
+    RA, decl = solar_polar_equatorial_coordinates(d)
+    print(f"solar_polar_equatorial_coordinates: RA = {RA}, decl = {decl}")
+
+    gsid = greenwich_sidereal_time(d)
+    print(f"greenwich_sidereal_time = {gsid}")
+
+    longitude = 15      # degrees
+    sid = local_sidereal_time(d, longitude)
+    print(f"local_sidereal_time = {sid}")
+
+    HA, declination = alternative_polar_equatorial_coordinates(d, longitude)
+    print(f"alternative_polar_equatorial_coordinates: HA = {HA}, declination = {declination}")
+
+    latitude = 60  # degrees
+    azimuth, altitude = solar_horizontal_coordinates(d, latitude, longitude)
+    print(f"azimuth = {azimuth}, altitude = {altitude}")
